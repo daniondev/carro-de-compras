c76be387baf6330eff189b97efba80fd700beb52
danielwish <daniondev@gmail.com>
2022-05-31 00:12:37 -0400

    Agregar Items al carro y mostrarlos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import Productos from './components/Productos'
 import Layout from './components/Layout'
 import Title from './components/Title'
+import Navbar from './components/Navbar'
 
 class App extends Component {
 	state = {
@@ -9,18 +10,56 @@ class App extends Component {
 			{ name: 'Tomate', price: 1500, img: '/productos/tomate.jpg' },
 			{ name: 'Arbejas', price: 1500, img: '/productos/arbejas.jpg' },
 			{ name: 'Lechuga', price: 1500, img: '/productos/lechuga.jpg' }
-		]
+		],
+		carro: [
+			//{ name: 'Tomate', price: 1500, img: '/productos/tomate.jpg', cantidad: 1 },
+		], 
+		esCarroVisible: false
+	}
+
+	agregarAlCarro = (producto) => {
+		const { carro } = this.state
+
+		if(carro.find(x => x.name === producto.name)){
+			const newCarro = carro.map(x => x.name === producto.name
+				? ({
+					...x,
+					cantidad: x.cantidad + 1
+				})
+				: x)
+			return this.setState({ carro : newCarro })	
+		}
+
+		return this.setState({
+			carro: this.state.carro.concat({
+				...producto,
+				cantidad: 1
+			})
+		})
+	}
+
+	mostrarCarro = () => {
+		if(!this.state.carro.length)
+			return false
+
+		this.setState({ esCarroVisible : !this.state.esCarroVisible })
 	}
   
 	render(){
+		const { esCarroVisible } = this.state
+
 		return (
 			<div>
+			<Navbar 
+				carro={this.state.carro} 
+				esCarroVisible={esCarroVisible} 
+				mostrarCarro={this.mostrarCarro} />
 			<Layout>
 				<Title 
 				
 				/>
 				<Productos
-				agregarAlCarro={() => console.log('No hace nada')}
+				agregarAlCarro={this.agregarAlCarro}
 				productos={this.state.productos}
 				/> 
 			</Layout>
